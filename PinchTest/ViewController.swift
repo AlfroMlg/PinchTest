610b7fa61f8ad93992dbba01a5c2d52572eabcc4
Alfredo <col.alfredo@gmail.com>
2022-02-07 21:36:27 +0100

    initial commit

---
+++ PinchTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PinchTest
+//
+//  Created by Alfredo Martin-Hinojal Acebal on 7/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
