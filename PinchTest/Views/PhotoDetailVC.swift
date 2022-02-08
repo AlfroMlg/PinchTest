b048408d51933adff8714d368a5afbfbc62089c9
Alfredo <col.alfredo@gmail.com>
2022-02-08 17:40:11 +0100

    Adds view for photodetail -m  Adds test for PhotosViewModel

---
+++ PinchTest/Views/PhotoDetailVC.swift
@@ -0,0 +1,8 @@
+//
+//  PhotoDetailVC.swift
+//  PinchTest
+//
+//  Created by Alfredo Martin-Hinojal Acebal on 8/2/22.
+//
+
+import Foundation
---
+++ PinchTestTests/ViewModel/PhotosViewModelTests.swift
@@ -0,0 +1,75 @@
+//
+//  PhotosViewModel.swift
+//  PinchTestTests
+//
+//  Created by Alfredo Martin-Hinojal Acebal on 8/2/22.
+//
+
+import XCTest
+@testable import PinchTest
+class PhotosViewModel: XCTestCase {
+    var sut = PhotosViewModel(networkManager: NetworkManager<DataType>())
+    var mockUrlSession = MockURLSession()
+    let userID = 1
+    let id = 1
+    let title: String = "Test album"
+    lazy var responseDict : [String : Any] = ["id":id, "userId":userID, "title":title]
+
+    override func setUpWithError() throws {
+        sut.networkManager.session = mockUrlSession
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testCallToData() throws {
+        //when
+        
+        var jsonArray = [[String: Any]]()
+        jsonArray.append(responseDict)
+
+        let responseData = try! JSONSerialization.data(withJSONObject: jsonArray, options: [])
+        
+        //fetch data
+        sut.callToData(dataType: DataType.albums(page: 1), result: [Album].self)
+        mockUrlSession.completionHandler?(responseData, nil ,nil)
+        
+        //then
+        XCTAssertNotNil(mockUrlSession.completionHandler)
+    }
+    
+    func testUserDataRequest_CreatesAlbum() {
+        //when
+        //create fake album
+        var testAlbum : [Album]?
+        
+        var jsonArray = [[String: Any]]()
+        jsonArray.append(responseDict)
+
+        let responseData = try! JSONSerialization.data(withJSONObject: jsonArray, options: [])
+        
+        let completion = { (results:Result<[Album], Error>) in
+            switch results {
+            case .success(let album):
+                testAlbum = album
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+        }
+        //fetch data
+        sut.networkManager.request(route: DataType.albums(page: 1), completion: completion)
+        mockUrlSession.completionHandler?(responseData, nil ,nil)
+        
+        //then
+        XCTAssertNotNil(testAlbum)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
